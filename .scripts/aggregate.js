51f68fb028dcfb90613aa6c776852381bcc461ea
Hagen <hagen@hyena.gay>
2022-07-25 17:11:41 +0100

    feat: crowdin

--- .scripts/aggregate.js
+++ .scripts/aggregate.js
@@ -1,48 +1,75 @@
 // @ts-nocheck
 'use strict';
 
-const data = require('../index');
+const path = require('node:path');
+const { readFileSync } = require('node:fs');
+const { parseExpression } = require('@babel/parser');
+const data = require('../data');
 const dot = require('dot-object');
-const fs = require('node:fs');
+const { sourceLocale } = require('../constants');
+const { findJsonNode, getLocaleFiles } = require('../util');
 
-// ignore config files
-for (const code in data) delete data[code].locale;
-const english = new Intl.DisplayNames('en', { type: 'language' });
-const text = [];
+const base = path.resolve(__dirname, '..');
 
-for (const code in data) {
+const formatLineFragment = (start, end) => `L${start}${end && end !== start ? `-L${end}` : ''}`;
+
+const uriEncodeFilename = filename => filename.split(path.sep).map(encodeURIComponent).join('/');
+
+const formatEntryInfo = (locale, entry) => {
+  const info = this.entryInfo(locale, entry);
+  const sourceInfo = this.entryInfo(sourceLocale, entry);
+  const baseUrl = 'https://github.com/OfficialPawBot/language/blob/main/';
+
+  let text = info ? `* [\`${entry}\`](${baseUrl}${uriEncodeFilename(info.filename)}#${formatLineFragment(info.start.line, info.end.line)})` :
+    `* \`${entry}\``
+
+  if (sourceInfo) text += ` [Source](${baseUrl}${uriEncodeFilename((sourceInfo.filename))}#${formatLineFragment(sourceInfo.start.line, sourceInfo.end.line)})`
+
+  return text;
+}
+
+const astCache = {};
+exports.entryInfo = (locale, entry) => {
+  const entryKeys = entry.replace(/\[(-?\d+)\]/g, '.$1').split('.');
+  for (const file of getLocaleFiles([locale])) {
+    const keys = file.split(path.sep).slice(1);
+    if (keys.some((v, i) => v !== entryKeys[i])) continue;
+    const filename = `${file}.json`;
+    let index = keys.length;
+
+    const data = readFileSync(path.join(base, filename), 'utf8');
+    const ast = astCache[filename] ??= parseExpression(data, {
+      ranges: true,
+      sourceFilename: filename,
+    });
+    if (index === entryKeys.length) return ast.loc;
+
+    const node = findJsonNode(ast, entryKeys, index);
+    return node?.loc;
+  }
+}
+
+exports.aggregate = locale => {
   const summary = {
     missing: [],
     noent: [],
   };
-  for (const key in dot.dot(data.en)) {
-    if (!dot.pick(key, data[code])) {
-      summary.missing.push(key);
-    }
-  }
-  for (const key in dot.dot(data[code])) {
-    const origin = dot.pick(key, data.en);
-    if (!origin) {
-      summary.noent.push(key);
-    }
-  }
-  if (code !== 'en') {
-    const display = new Intl.DisplayNames(code, { type: 'language' });
-    text.push(
-      `## \`${code}\` ${english.of(code)} (${display.of(code).replace(/./, r => r.toUpperCase())})${
-        summary.missing.length === 0 && summary.noent.length === 0 ? '\n\nCompleted locale.' : `${
-        summary.missing.length !== 0 ? `\n\nMissing the following entries:\n\n${summary.missing.map(t => `* \`${t}\``).join('\n')}` : ''}${
-        summary.noent.length !== 0 ? `\n\nContains deprecated entries:\n\n${summary.noent.map(t => `* \`${t}\``).join('\n')}` : ''
-      }`}`
-    );
-  }
-}
 
-fs.writeFileSync('./AGGREGATE.md',
-  fs.readFileSync('./AGGREGATE.md')
-    .toString()
-    .replace(
-      /<!-- BLOCK AGGREGATE BEGIN -->[\s\S]+<!-- BLOCK AGGREGATE END -->/,
-      '<!-- BLOCK AGGREGATE BEGIN -->\n\n' + text.join('\n\n') + '\n\n<!-- BLOCK AGGREGATE END -->'
-    )
-)
+  for (const key in dot.dot(data[sourceLocale]))
+    if (!dot.pick(key, data[locale]))
+      summary.missing.push(key);
+
+  for (const key in dot.dot(data[locale])) {
+    const origin = dot.pick(key, data[sourceLocale]);
+    if (!origin) summary.noent.push(key);
+  }
+
+  const text = [];
+  if (summary.missing.length === 0 && summary.noent.length === 0) return 'This locale is fully translated, but may require further proofreading.';
+  else {
+    if (summary.missing.length !== 0) text.push(`Missing the following entries:\n\n${summary.missing.map(t => formatEntryInfo(locale, t)).join('\n')}`);
+    if (summary.noent.length !== 0) text.push(`Contains deprecated entries:\n\n${summary.noent.map(t => formatEntryInfo(locale, t)).join('\n')}`);
+  }
+
+  return text.join('\n\n');
+};
--- .scripts/lint.js
+++ .scripts/lint.js
@@ -1,33 +1,61 @@
 'use strict';
 
 const fs = require('node:fs');
-const glob = require('glob');
-const { format, resolveConfigFile, resolveConfig } = require('prettier');
+const path = require('node:path');
+const { isDeepStrictEqual } = require('node:util');
+const { format, resolveConfigFile, resolveConfig, check } = require('prettier');
+const mri = require('mri');
+const { getLocaleFiles } = require('../util');
+const { locales } = require('../constants');
 
-const config = resolveConfig.sync(resolveConfigFile.sync());
-for (const file of glob.sync('./??/*.json')) {
-  const content = fs.readFileSync(file).toString();
-  const parsed = JSON.parse(content); // parsable? good enough
-  if (process.argv.slice(2).includes('--fix')) {
-    const sorted = recursiveSort(parsed);
-    const data = JSON.stringify(sorted, null, 2);
-    const formatted = format(data, { ...config, parser: 'json' });
-    fs.writeFileSync(file, formatted);
-  }
-}
+const base = path.resolve(__dirname, '..');
 
-function recursiveSort(object) {
+const recursiveSort = object => {
   const sorted = {};
+
   for (
     const key of Object
       .keys(object)
-      .sort(
-        (a, b) => a.normalize().localeCompare(b.normalize())
-      )
+      .sort((a, b) => a.normalize().localeCompare(b.normalize()))
   ) {
     if (object[key] && typeof object[key] === 'object' && !Array.isArray(object[key])) {
       sorted[key] = recursiveSort(object[key])
     } else sorted[key] = object[key];
   }
+
   return sorted;
+};
+
+const run = () => {
+  const options = mri(process.argv.slice(2));
+  const configPath = resolveConfigFile.sync(base);
+  if (!configPath) throw Error('.prettierrc not found. Did you accidentally delete it?');
+  const config = resolveConfig.sync(configPath);
+
+  let unformattedFiles = false;
+  for (const file of getLocaleFiles(locales)) {
+    const filename = `${file}.json`;
+    const filepath = path.join(base, filename);
+    const content = fs.readFileSync(filepath, 'utf8');
+    const data = require(`../${file}.json`);
+    let sorted = recursiveSort(data);
+
+    if (options.fix) {
+      const str = JSON.stringify(sorted, null, 2);
+      const formatted = format(str, { ...config, filepath });
+      fs.writeFileSync(filepath, formatted);
+    } else {
+      if (!check(content, { ...config, filepath }) || !isDeepStrictEqual(data, sorted)) {
+        console.warn(filename);
+        unformattedFiles = true;
+      }
+    }
+  }
+
+  if (unformattedFiles) {
+    console.warn('Unformatted files found. Try running lint:fix');
+    process.exitCode = 1;
+  }
 }
+
+run();
