82f4a5eb9b4ba4fbeedfe9a413ca48c15bc42f81
Hagen <hagen@hyena.gay>
2024-02-27 05:58:42 +0000

    refactor: language disambiguation needed

--- constants.js
+++ constants.js
@@ -3,3 +3,7 @@ const { getLocaleDirents } = require('./util');
 exports.locales = getLocaleDirents().map(e => e.name);
 exports.sourceLocale = 'en-GB';
 exports.localeConfigName = 'locale';
+exports.preferences = {
+  // language disambiguation, e.g. if both `en-GB` and `en-US` exist, this should include:
+  // en: "en-GB"
+};
